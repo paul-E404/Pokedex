81f6dc588e8af80b2aecebc25996c2e37abf4fae
paul-E404 <paul_engerling@web.de>
2021-04-27 13:00:08 +0200

    Continued CSS pokédex entry styling (type fire)

--- script.js
+++ script.js
@@ -23,3 +23,41 @@ function renderPokémonInfo() {
     pokémonNumber.innerHTML = '#00' + currentPokémon.game_indices[6].game_index;
     pokémonImage.innerHTML = `<img src="${currentPokémon.sprites.other['official-artwork'].front_default}" alt="picture-${currentPokémon.name}">`;
 }
+
+
+function showInfoTable(info) {
+    removeHighlighting(info);
+    if(info == 'about') {
+        document.getElementById('about-table').style.transform = 'translateX(0)';
+        document.getElementById('base-stats-table').style.transform = 'translateX(calc(var(--pokémon-entry-width) + 40px))';
+        document.getElementById('moves-table').style.transform = 'translateX(calc(var(--pokémon-entry-width)*2 + 80px))';
+    }
+    else if (info == 'base-stats') {
+        document.getElementById('about-table').style.transform = 'translateX(calc(var(--pokémon-entry-width) * (-1) - 40px))';
+        document.getElementById('base-stats-table').style.transform = 'translateX(0)';
+        document.getElementById('moves-table').style.transform = 'translateX(calc(var(--pokémon-entry-width) + 40px))';
+    }
+    else if (info == 'moves') {
+        document.getElementById('about-table').style.transform = 'translateX(calc(var(--pokémon-entry-width) * (-2) - 80px))';
+        document.getElementById('base-stats-table').style.transform = 'translateX(calc(var(--pokémon-entry-width) * (-1) - 40px))';
+        document.getElementById('moves-table').style.transform = 'translateX(0)';
+    }
+}
+
+function removeHighlighting(info) {
+    if(info == 'about') {
+        document.getElementById('about').classList.remove('noHighlighting');
+        document.getElementById('base-stats').classList.add('noHighlighting');
+        document.getElementById('moves').classList.add('noHighlighting');
+    }
+    else if (info == 'base-stats') {
+        document.getElementById('about').classList.add('noHighlighting');
+        document.getElementById('base-stats').classList.remove('noHighlighting');
+        document.getElementById('moves').classList.add('noHighlighting');
+    }
+    else if (info == 'moves') {
+        document.getElementById('about').classList.add('noHighlighting');
+        document.getElementById('base-stats').classList.add('noHighlighting');
+        document.getElementById('moves').classList.remove('noHighlighting');
+    }
+}
