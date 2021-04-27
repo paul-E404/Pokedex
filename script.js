8e61b6fc9dd86dba8b17e9cec033c77fdaa5b4b3
paul-E404 <paul_engerling@web.de>
2021-04-27 17:16:22 +0200

    Finished fire type design

--- script.js
+++ script.js
@@ -11,17 +11,54 @@ async function loadPokémon() {
 
 function renderPokémonInfo() {
 
-    let pokédex = document.getElementById('pokédex');
+    /* let pokédex = document.getElementById('pokédex'); */
+    let pokédexName = document.getElementById('pokédex-name');
+    let pokédexTypeBtnSlot1 = document.getElementById('pokédex-type-btn-slot1');
+    let pokédexTypeBtnSlot2 = document.getElementById('pokédex-type-btn-slot2');
+    let pokédexImageBox = document.getElementById('pokédex-image-box');
+
+
     let pokémonName = document.getElementById('pokémon-name');
     let pokémonNumber = document.getElementById('pokémon-number');
-    let pokémonImage = document.getElementById('pokémon-image');
+    let pokémonTypeBtnSlot1 = document.getElementById('pokémon-type-btn-slot1');
+    let pokémonTypeBtnSlot2 = document.getElementById('pokémon-type-btn-slot2');
+    let pokémonImageBox = document.getElementById('pokémon-image-box');
 
-    pokédex.innerHTML += `<h2>${currentPokémon.name}</h2>`;
-    pokédex.innerHTML += `<img src="${currentPokémon.sprites.other['official-artwork'].front_default}" alt="picture-${currentPokémon.name}">`;
+    pokédexName.innerHTML = upperCaseFirstLetter(currentPokémon.name);
+    pokédexTypeBtnSlot1.innerHTML = upperCaseFirstLetter(currentPokémon.types[0].type['name']);
+    if(currentPokémon.types.length > 1) {
+        pokédexTypeBtnSlot2.classList.remove('d-none');
+        pokédexTypeBtnSlot2.innerHTML = upperCaseFirstLetter(currentPokémon.types[1].type['name']);
+    }
+    pokédexImageBox.innerHTML = `<img src="${currentPokémon.sprites.other['official-artwork'].front_default}" alt="picture-${currentPokémon.name}">`;
 
-    pokémonName.innerHTML = currentPokémon.name.charAt(0).toUpperCase() + currentPokémon.name.slice(1);
+ /*    pokédex.innerHTML += `<h2>${currentPokémon.name}</h2>`;
+    pokédex.innerHTML += `<img src="${currentPokémon.sprites.other['official-artwork'].front_default}" alt="picture-${currentPokémon.name}">`; */
+
+    pokémonTypeBtnSlot1.innerHTML = upperCaseFirstLetter(currentPokémon.types[0].type['name']);
+    if(currentPokémon.types.length > 1) {
+        pokémonTypeBtnSlot2.classList.remove('d-none');
+        pokémonTypeBtnSlot2.innerHTML = upperCaseFirstLetter(currentPokémon.types[1].type['name']);
+    }
+
+    pokémonName.innerHTML = upperCaseFirstLetter(currentPokémon.name);
     pokémonNumber.innerHTML = '#00' + currentPokémon.game_indices[6].game_index;
-    pokémonImage.innerHTML = `<img src="${currentPokémon.sprites.other['official-artwork'].front_default}" alt="picture-${currentPokémon.name}">`;
+    pokémonImageBox.innerHTML = `<img src="${currentPokémon.sprites.other['official-artwork'].front_default}" alt="picture-${currentPokémon.name}">`;
+}
+
+function upperCaseFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+
+function openEntry() {
+    let pokédexSingle = document.getElementById('pokédex-single');
+    pokédexSingle.classList.remove('d-none');
+}
+
+function closeEntry() {
+    let pokédexSingle = document.getElementById('pokédex-single');
+    pokédexSingle.classList.add('d-none');
 }
 
 
