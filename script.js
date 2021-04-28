9e8f94ebf7664dddf43ae6da759c543818c36bdc
paul-E404 <paul_engerling@web.de>
2021-04-28 10:24:13 +0200

    Loaded all pokédex entries via API request

--- script.js
+++ script.js
@@ -1,20 +1,57 @@
 let currentPokémon;
+let pkmnList;
+let id;
+let idList = [];
 
-async function loadPokémon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
+async function init() {
+    let url = 'https://pokeapi.co/api/v2/pokemon?limit=151&offset=0';
+    let response = await fetch(url);
+    pkmnList = await response.json();
+
+    for (let i = 0; i < pkmnList.results.length; i++) {
+        id = i;
+        idList.push(id);
+        currentPokémonURL = pkmnList.results[i].url;
+        console.log(currentPokémonURL);
+        loadPokémon(id, currentPokémonURL);
+    }
+}
+
+async function loadPokémon(id, currentPokémonURL) {
+    let url = currentPokémonURL;
     let response = await fetch(url);
     currentPokémon = await response.json();
 
     console.log(currentPokémon);
-    renderPokédexInfo();
+
+    showPokédex(id);
+    renderPokédexInfo(id);
 }
 
-function renderPokédexInfo() {
+function showPokédex(id) {
+    let pokédex = document.getElementById('pokédex');
+    pokédex.innerHTML += generateHTMLForPokédex(id);
+    renderPokédexInfo(id);
+}
 
-    let pokédexName = document.getElementById('pokédex-name');
-    let pokédexTypeBtnSlot1 = document.getElementById('pokédex-type-btn-slot1');
-    let pokédexTypeBtnSlot2 = document.getElementById('pokédex-type-btn-slot2');
-    let pokédexImageBox = document.getElementById('pokédex-image-box');
+function generateHTMLForPokédex(id) {
+    return `<div onclick="openEntry(${id});" id="${id}" class="pokédex-element">
+                <div id="pokédex-name-${id}" class="pokédex-name"></div>
+                <div class="pokédex-type-box">
+                    <button id="pokédex-type-btn-slot1-${id}" class="type-btn fire-btn"></button>
+                    <button id="pokédex-type-btn-slot2-${id}" class="type-btn d-none"></button>
+                </div>
+                <div id="pokédex-image-box-${id}" class="pokédex-image-box">
+                </div>
+            </div>`;
+}
+
+function renderPokédexInfo(id) {
+
+    let pokédexName = document.getElementById(`pokédex-name-${id}`);
+    let pokédexTypeBtnSlot1 = document.getElementById(`pokédex-type-btn-slot1-${id}`);
+    let pokédexTypeBtnSlot2 = document.getElementById(`pokédex-type-btn-slot2-${id}`);
+    let pokédexImageBox = document.getElementById(`pokédex-image-box-${id}`);
 
     pokédexName.innerHTML = upperCaseFirstLetter(currentPokémon.name);
     pokédexTypeBtnSlot1.innerHTML = upperCaseFirstLetter(currentPokémon.types[0].type['name']);
@@ -33,8 +70,8 @@ function upperCaseFirstLetter(string) {
 }
 
 
-function openEntry() {
-    renderPokémonInfo();
+function openEntry(id) {
+    showPokémon(id);
     let pokédexSingle = document.getElementById('pokédex-single');
     pokédexSingle.classList.remove('d-none');
 }
@@ -44,15 +81,144 @@ function closeEntry() {
     pokédexSingle.classList.add('d-none');
 }
 
-function renderPokémonInfo() {
+function showPokémon(id) {
+    let pokédexSingle = document.getElementById('pokédex-single');
+    pokédexSingle.innerHTML = generateHTMLForSingleEntry(id);
+    renderPokémonInfo(id);
+}
+
+function generateHTMLForSingleEntry(id) {
+    return `<img id="pokémon-entry-background-picture-${id}" class="pokémon-entry-background-picture" src="img/fire.png" alt="">
+            <div class="pokémon-entry">
+                <div id="pokémon-entry-background-layer-${id}" class="pokémon-entry-background-layer">
+
+                </div>
+                <div class="arrow-zone">
+                    <i class="fas fa-chevron-left"></i>
+                    <i onclick="closeEntry();" class="fas fa-times"></i>
+                    <i class="fas fa-chevron-right"></i>
+                </div>
+                <div id="pokémon-number-${id}" class="pokémon-number">
+
+                </div>
+                <div id="pokémon-name-${id}" class="pokémon-name">
+
+                </div>
+                <div id="pokémon-type-box" class="pokémon-type-box">
+                    <button id="pokémon-type-btn-slot1-${id}" class="type-btn fire-btn">Fire</button>
+                    <button id="pokémon-type-btn-slot2-${id}" class="type-btn d-none">Other</button>
+                </div>
+                <div id="pokémon-image-box-${id}" class="pokémon-image-box">
+
+                </div>
+                <div class="pokémon-info">
+                    <div class="pokémon-info-headlines">
+                        <div onclick="showInfoTable('about');" id="about">About</div>
+                        <div onclick="showInfoTable('base-stats');" id="base-stats" class="noHighlighting">Base Stats</div>
+                        <div onclick="showInfoTable('moves');" id="moves" class="noHighlighting">Moves</div>
+                    </div>
+
+                    <table id="about-table" class="about-table info-table">
+                        <tbody>
+                            <tr>
+                                <td>Height</td>
+                                <td id="pkmn-height-${id}"></td>
+                            </tr>
+                            <tr>
+                                <td>Weight</td>
+                                <td id="pkmn-weight-${id}"></td>
+                            </tr>
+                            <tr>
+                                <td>Abilities</td>
+                                <td id="pkmn-abilities-${id}"></td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <table id="base-stats-table" class="base-stats-table info-table">
+                        <tbody>
+                            <tr>
+                                <td>HP</td>
+                                <td id="hp-${id}"></td>
+                                <td>
+                                    <div class="base-stats-bar-container">
+                                        <div id="hp-bar" class="base-stats-bar-value"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Attack</td>
+                                <td id="attack-${id}"></td>
+                                <td>
+                                    <div class="base-stats-bar-container">
+                                        <div id="attack-bar" class="base-stats-bar-value"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Defense</td>
+                                <td id="defense-${id}"></td>
+                                <td>
+                                    <div class="base-stats-bar-container">
+                                        <div id="defense-bar" class="base-stats-bar-value"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Sp. Atk</td>
+                                <td id="sp-atk-${id}"></td>
+                                <td>
+                                    <div class="base-stats-bar-container">
+                                        <div id="sp-atk-bar" class="base-stats-bar-value"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Sp. Def</td>
+                                <td id="sp-def-${id}"></td>
+                                <td>
+                                    <div class="base-stats-bar-container">
+                                        <div id="sp-def-bar" class="base-stats-bar-value"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Speed</td>
+                                <td id="speed-${id}"></td>
+                                <td>
+                                    <div class="base-stats-bar-container">
+                                        <div id="speed-bar" class="base-stats-bar-value"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <table id="moves-table" class="moves-table info-table">
+                        <thead>
+                            <tr>
+                                <td>Level</td>
+                                <td>Move</td>
+                            </tr>
+                        </thead>
+                        <tbody id="moves-table-tbody">
+
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>`
+}
+
+function renderPokémonInfo(id) {
 
     console.log("renderPokémonInfo() wird aufgerufen!");
 
-    let pokémonName = document.getElementById('pokémon-name');
-    let pokémonNumber = document.getElementById('pokémon-number');
-    let pokémonTypeBtnSlot1 = document.getElementById('pokémon-type-btn-slot1');
-    let pokémonTypeBtnSlot2 = document.getElementById('pokémon-type-btn-slot2');
-    let pokémonImageBox = document.getElementById('pokémon-image-box');
+    let pokémonName = document.getElementById(`pokémon-name-${id}`);
+    let pokémonNumber = document.getElementById(`pokémon-number-${id}`);
+    let pokémonTypeBtnSlot1 = document.getElementById(`pokémon-type-btn-slot1-${id}`);
+    let pokémonTypeBtnSlot2 = document.getElementById(`pokémon-type-btn-slot2-${id}`);
+    let pokémonImageBox = document.getElementById(`pokémon-image-box-${id}`);
 
     pokémonName.innerHTML = upperCaseFirstLetter(currentPokémon.name);
     pokémonNumber.innerHTML = '#00' + currentPokémon.game_indices[6].game_index;
@@ -63,18 +229,18 @@ function renderPokémonInfo() {
     }
     pokémonImageBox.innerHTML = `<img src="${currentPokémon.sprites.other['official-artwork'].front_default}" alt="picture-${currentPokémon.name}">`;
 
-    renderAbout();
-    renderBaseStats();
+    renderAbout(id);
+    renderBaseStats(id);
     renderMoves();
 
 }
 
 
-function renderAbout() {
+function renderAbout(id) {
 
-    let pkmnHeight = document.getElementById('pkmn-height');
-    let pkmnWeight = document.getElementById('pkmn-weight');
-    let pkmnAbilities = document.getElementById('pkmn-abilities');
+    let pkmnHeight = document.getElementById(`pkmn-height-${id}`);
+    let pkmnWeight = document.getElementById(`pkmn-weight-${id}`);
+    let pkmnAbilities = document.getElementById(`pkmn-abilities-${id}`);
     let abilitiesArr = [];
 
     pkmnHeight.innerHTML = currentPokémon.height / 10 + " m";
@@ -88,14 +254,14 @@ function renderAbout() {
 }
 
 
-function renderBaseStats() {
+function renderBaseStats(id) {
 
-    let hp = document.getElementById('hp');
-    let attack = document.getElementById('attack');
-    let defense = document.getElementById('defense');
-    let spAtk = document.getElementById('sp-atk');
-    let spDef = document.getElementById('sp-def');
-    let speed = document.getElementById('speed');
+    let hp = document.getElementById(`hp-${id}`);
+    let attack = document.getElementById(`attack-${id}`);
+    let defense = document.getElementById(`defense-${id}`);
+    let spAtk = document.getElementById(`sp-atk-${id}`);
+    let spDef = document.getElementById(`sp-def-${id}`);
+    let speed = document.getElementById(`speed-${id}`);
 
     let hpValue = currentPokémon.stats[0].base_stat;
     let attackValue = currentPokémon.stats[1].base_stat;
@@ -115,24 +281,21 @@ function renderBaseStats() {
 }
 
 function renderBaseStatsBar(hpValue, attackValue, defenseValue, spAtkValue, spDefValue, speedValue) {
-    const maxHP = 255;
-    const maxAttack = 190;
-    const maxDefense = 230;
-    const maxSpAtk = 194;
-    const maxSpDef = 230;
-    const maxSpeed = 200;
 
-    document.getElementById('hp-bar').style.width = `calc(var(--pokémon-entry-width) * 0.6 * (${hpValue} / ${maxHP})`;
-    document.getElementById('attack-bar').style.width = `calc(var(--pokémon-entry-width) * 0.6 * (${attackValue} / ${maxAttack})`;
-    document.getElementById('defense-bar').style.width = `calc(var(--pokémon-entry-width) * 0.6 * (${defenseValue} / ${maxDefense})`;
-    document.getElementById('sp-atk-bar').style.width = `calc(var(--pokémon-entry-width) * 0.6 * (${spAtkValue} / ${maxSpAtk})`;
-    document.getElementById('sp-def-bar').style.width = `calc(var(--pokémon-entry-width) * 0.6 * (${spDefValue} / ${maxSpDef})`;
-    document.getElementById('speed-bar').style.width = `calc(var(--pokémon-entry-width) * 0.6 * (${speedValue} / ${maxSpeed})`;
+    const maxBaseStats = [{ "maxHP": 255 }, { "maxAttack": 190 }, { "maxDefense": 230 }, { "maxSpAtk": 194 }, { "maxSpDef": 230 }, { "maxSpeed": 200 }];
+
+    document.getElementById('hp-bar').style.width = `calc(var(--pokémon-entry-width) * 0.6 * (${hpValue} / ${maxBaseStats[0]['maxHP']})`;
+    document.getElementById('attack-bar').style.width = `calc(var(--pokémon-entry-width) * 0.6 * (${attackValue} / ${maxBaseStats[1]['maxAttack']})`;
+    document.getElementById('defense-bar').style.width = `calc(var(--pokémon-entry-width) * 0.6 * (${defenseValue} / ${maxBaseStats[2]['maxDefense']})`;
+    document.getElementById('sp-atk-bar').style.width = `calc(var(--pokémon-entry-width) * 0.6 * (${spAtkValue} / ${maxBaseStats[3]['maxSpAtk']})`;
+    document.getElementById('sp-def-bar').style.width = `calc(var(--pokémon-entry-width) * 0.6 * (${spDefValue} / ${maxBaseStats[4]['maxSpDef']})`;
+    document.getElementById('speed-bar').style.width = `calc(var(--pokémon-entry-width) * 0.6 * (${speedValue} / ${maxBaseStats[5]['maxSpeed']})`;
 
 }
 
 function renderMoves() {
-   
+
+    //JSON-Array with all levels and moves
     levelAndMovesArray = [];
 
     for (let i = 0; i < currentPokémon.moves.length; i++) {
@@ -142,13 +305,13 @@ function renderMoves() {
                 if (moveVersionRubySapphire(moveInfo, j)) {
                     let level = moveInfo[j].level_learned_at;
                     let move = currentPokémon.moves[i].move['name'];
-                    let levelAndMoveJSON = {"level" : level, "move" : move};
+                    let levelAndMoveJSON = { "level": level, "move": move };
                     levelAndMovesArray.push(levelAndMoveJSON);
                 }
             }
-            
         }
     }
+    compareNumbers(levelAndMovesArray);
     console.log("levelAndMovesArray", levelAndMovesArray);
     showMoves(levelAndMovesArray);
 }
@@ -163,13 +326,20 @@ function moveVersionRubySapphire(moveInfo, j) {
     return moveInfo[j].version_group['name'] == 'ruby-sapphire';
 }
 
+function compareNumbers(JSONArrayOfNumbers) {
+    JSONArrayOfNumbers.sort(function (a, b) {
+        a = a.level;
+        b = b.level;
+
+        return a - b;
+    });
+}
 
 function showMoves(levelAndMovesArray) {
     let movesTableTbody = document.getElementById('moves-table-tbody');
     for (let i = 0; i < levelAndMovesArray.length; i++) {
         let levelAndMove = levelAndMovesArray[i];
         movesTableTbody.innerHTML += generateHTMLForMovesTable(levelAndMove);
-        console.log("levelAndMove", levelAndMove);
     }
 }
 
